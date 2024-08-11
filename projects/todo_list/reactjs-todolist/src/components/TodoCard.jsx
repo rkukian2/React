c3b7eb7525a457a7f8fb6bcfcda4d6c67e5fe3ab
Rudra Kukian <rudrakukian@Rudras-MacBook-Pro-2.local>
2024-08-11 17:21:28 -0500

    Delete and Edit functionality added

--- projects/todo_list/reactjs-todolist/src/components/TodoCard.jsx
+++ projects/todo_list/reactjs-todolist/src/components/TodoCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-    const {children} = props
+    const {children, handleDeleteTodo, index, handleEditTodo} = props
   return (
     <li className='todoItem'>
+        {children}
         <div className='actionsContainer'>
-            {children}
-
-            <i className="fa-regular fa-pen-to-square"></i>
-            <i className="fa-regular fa-trash-can"></i>
+            <button onClick={() => {
+                handleEditTodo(index)
+            }}>
+                <i className="fa-regular fa-pen-to-square"></i>
+            </button>
+            <button onClick={() => {
+                handleDeleteTodo(index)
+            }}>
+                <i className="fa-regular fa-trash-can"></i>
+            </button>
         </div>
     </li>
   )
