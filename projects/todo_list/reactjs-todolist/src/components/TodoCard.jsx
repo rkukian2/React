07c2e7288d1f6d8ce2621331216b0817a4d01165
Rudra Kukian <rudrakukian@Rudras-MacBook-Pro-2.local>
2024-08-11 17:00:25 -0500

    Todo List working with add functionality

---
+++ projects/todo_list/reactjs-todolist/src/components/TodoCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+    const {children} = props
+  return (
+    <li className='todoItem'>
+        <div className='actionsContainer'>
+            {children}
+
+            <i className="fa-regular fa-pen-to-square"></i>
+            <i className="fa-regular fa-trash-can"></i>
+        </div>
+    </li>
+  )
+}
---
+++ projects/todo_list/reactjs-todolist/src/components/TodoInput.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function TodoInput(props) {
+    const {handleAddTodos} = props
+    const [todoValue, setTodoValue] = useState('')
+
+    return (
+        <header>
+            <input  value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter To Do..."/>
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
+}
