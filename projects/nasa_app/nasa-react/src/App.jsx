243611383385fe8ce930f025eed1ea2f6aa10b1d
Rudra Kukian <rudrakukian@Rudras-MacBook-Pro-2.local>
2024-08-12 17:49:21 -0500

    Nasa App Setup

---
+++ projects/nasa_app/nasa-react/src/App.jsx
@@ -0,0 +1,15 @@
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+import SideBar from "./components/Sidebar"
+
+function App() {
+  return (
+    <>
+    <SideBar></SideBar>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
